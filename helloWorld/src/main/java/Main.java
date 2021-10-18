2a97235998a236df2e57eaabdc50a462d9ad55dd
mechanin <mechanin2000@gmail.com>
2021-10-18 12:43:42 +0100

    Added a print statement printing Ola to console

--- helloWorld/src/main/java/Main.java
+++ helloWorld/src/main/java/Main.java
@@ -1,6 +1,20 @@
 public class Main {
+    //    Default method setup for any class
     public static void main(String[] args) {
+//        Print to Console
         System.out.println("Hello World!");
         System.out.println("Ola");
+        System.out.println();
+//        Variable types
+        String name = "Helena";
+        System.out.println(name);
+
+        int age = 20;
+        long age2 = 20L;
+
+        double pi = 3.14;
+        float pi2 = 3.14f;
+
+        char letter = 'A';
     }
 }
